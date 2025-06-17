31252f18c5f2a4ac3518ceecc3b4053cde21d644
phpablo <pablohenrique120@gmail.com>
2025-06-17 09:28:21 -0300

    :pencil: Aula 02 - Programação com JavaScript
    Conceitos iniciais
    Declaração de variáveis e suas diferenças

---
+++ mod_1A_desenvolvimento_dinamico/ex_02_nodeexec/main.js
@@ -0,0 +1,13 @@
+console.log('Hello World')
+let num1 = 3
+let num2 = 4
+
+let soma = num1 + num2
+console.log(`A soma entre ${num1} e ${num2} é ${soma}`)
+
+// Exemplos de declaração de variáveis em JavaScript:
+var x = 10;        // variável com escopo de função
+let y = 20;        // variável com escopo de bloco
+const z = 30;      // constante (não pode ser reatribuída)
+let name = "Alice";
+const pi = 3.14159;

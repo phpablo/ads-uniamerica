b516b1a5b3f6d663b100b27dc7da94503f636dc9
phpablo <pablohenrique120@gmail.com>
2025-06-18 14:40:22 -0300

    :pencil: Tipos de variáveis

---
+++ mod_1A_desenvolvimento_dinamico/ex_03_primeiro_comando/main.js
@@ -0,0 +1,14 @@
+let x
+let y = "String"
+let z = []
+let j = 7
+let l = 7.2
+let k = new Array()
+
+console.log(typeof(x)+'\n')
+console.log(typeof(y)+'\n')
+console.log(typeof(z)+'\n')
+console.log(typeof(k)+'\n')
+console.log(typeof(j)+'\n')
+console.log(typeof(l)+'\n')
+
